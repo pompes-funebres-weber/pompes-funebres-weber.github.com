6994b03938094848489e624449bf45adea0e1227
Godu <arthur.weber@viacesi.fr>
2012-09-06 15:55:39 +0200

    Fix caroussel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
- $(function(){
+
+
+
+$(function(){
   new (Backbone.Router.extend({
     initialize: function() {
+      $(window).resize(this.resize);
+    },
+
+    resize: function() {
+      $('.carousel').each(function(){
+        $(this).find('.item').css('height', $(this).width() * 3/4)
+        .css('width', $(this).width())
+        .css('line-height', $(this).height() + 'px');
+      });
     },
 
     show: function(tab) {
@@ -16,6 +28,7 @@
         this.show('home');
       else
         this.show(action);
+      this.resize();
     }
   }))();
 
